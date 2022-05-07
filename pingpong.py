166b013578e3f9eed2115d6d3ffc5fa9c76d73eb
Retr0V1z0r <50799328+Br0fer@users.noreply.github.com>
2022-05-07 17:11:23 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -39,8 +39,48 @@ game = True
 clock = time.Clock()
 FPS = 60
 
-racket1 = Player("racket.png", 30, 200, 4, 25, 75)
-racket2 = Player("racket.png", 520, 200, 4, 25, 75)
-ball = GameSprite("")
+racket1 = Player(img_racket, 30, 200, 4, 25, 75)
+racket2 = Player(img_racket, 520, 200, 4, 25, 75)
+ball = GameSprite(img_ball, 200, 200, 4, 50, 50)
+
+font.init()
+font = fontFont(None, 35)
+lose1 = font.render("Player1 LOSE!",  True, (180, 0, 0))
+lose2 = font.render("Player2 LOSE!",  True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+
 
 while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        background = transform.scale(image.load(img_back), (win_width, win_height))
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= -1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            game = False
+            window.blit(lose1, (200, 200))
+        if ball.rect.x > 600:
+            finish = True
+            game = False
+            window.blit(lose2, (200, 200))
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+        
+    display.update()
+    clock.tick(FPS)
+            
+            
